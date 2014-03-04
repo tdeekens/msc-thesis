37a99868798345eb37ed0c9b0a4c0613e87a7312
anna-averianova <ann.averyanova@gmail.com>
2014-03-04 16:20:24 +0100

    Includes final data for day 1

--- heatmaps/R-scr/heatmap-script.R
+++ heatmaps/R-scr/heatmap-script.R
@@ -8,7 +8,7 @@ library(lattice)
 #########################################################
 ### Read data from .csv file
 #########################################################
-data <- read.csv("../ms2-d1.csv", comment.char="",header=TRUE, row.names=1, sep=";")
+data <- read.csv("../ms2-d1-grouped.csv", comment.char="",header=TRUE, row.names=1, sep=";")
 
 #########################################################
 ### Save column and row names in a separate variable
@@ -18,7 +18,7 @@ rcNames <- list(row.names(data), colnames(data))
 #########################################################
 ### Trransform data into a matrix
 #########################################################
-cdata <- matrix(as.numeric(unlist(data)), ncol = 22, byrow = FALSE, dimnames = rcNames)
+cdata <- matrix(as.numeric(unlist(data)), nrows=11, ncol = 25, byrow = FALSE, dimnames = rcNames)
 
 #########################################################
 ### Color palette to use
@@ -39,7 +39,7 @@ matrix_length <- list(0:(ncol(cdata)))
 
 heatmap.2(cdata, dendrogram="none", Rowv= NULL,      		  # No dendrogram
 			symm=TRUE, 	trace="none", density.info = "none",  # Treat matrix symmetrically
-			main = "Heat Map #1", srtCol=45, col=trypalette,  # Write Column names with 45 degrees angle
+			main = "Heat Map #1", srtCol=45, col=otherpalette,  # Write Column names with 45 degrees angle
 			sepwidth=c(0.01, 0.01), sepcolor="black", colsep=matrix_length, rowsep=matrix_length # Specify where to draw borders
 			)
 
