c7c0a97bd702cd84535dca91be1c6293f1bbca3f
anna-averianova <ann.averyanova@gmail.com>
2014-03-07 09:25:47 +0100

    Includes new names and codes

--- heatmaps/R-scr/heatmap-script.R
+++ heatmaps/R-scr/heatmap-script.R
@@ -9,7 +9,7 @@ library(lattice)
 ### Read data from .csv file
 ### Change the path to your data file
 #########################################################
-data <- read.csv("../annaav/Documents/empty-hm.csv", comment.char="",header=TRUE, row.names=1, sep=";")
+data <- read.csv("../d3-all-avgc.csv", comment.char="",header=TRUE, row.names=1, sep=";")
 
 #########################################################
 ### Save column and row names in a separate variable
@@ -19,15 +19,15 @@ rcNames <- list(row.names(data), colnames(data))
 #########################################################
 ### Trransform data into a matrix
 #########################################################
-cdata <- matrix(as.numeric(unlist(data)), ncol = 25, nrow=11, byrow = FALSE, dimnames = rcNames)
+cdata <- matrix(as.numeric(unlist(data)), ncol = 23, nrow=6, byrow = FALSE, dimnames = rcNames)
 
 #########################################################
 ### Color palette to use
 #########################################################
-mypalette<-brewer.pal(7,"Reds")
+mypalette<-brewer.pal(40,"Reds")
 
 ### Alternative (green to red)
-otherpalette <- colorRampPalette(c("green", "yellow", "red"))(n = 299)
+otherpalette <- colorRampPalette(c("green", "yellow", "red"))(n = 34)
 
 #########################################################
 ### Save the dimension of the matrix in a separate variable
@@ -35,7 +35,7 @@ otherpalette <- colorRampPalette(c("green", "yellow", "red"))(n = 299)
 matrix_length <- list(0:(ncol(cdata)))
 
 #########################################################
-### Draw a heatmap
+### Comment one of the following commands out and draw a heatmap
 #########################################################
 
 heatmap.2(cdata, trace="none", density.info = "none",
@@ -43,8 +43,13 @@ heatmap.2(cdata, trace="none", density.info = "none",
 			main = "Heat Map #1", srtCol=45, col=mypalette,  # Write Column names with 45 degrees angle
 			sepwidth=c(0.01, 0.01), sepcolor="black", colsep=matrix_length, rowsep=matrix_length # Specify where to draw borders
 			)
-
+### Take 2 on clustering
+heatmap.2(cdata, trace="none", density.info = "none",
+			Rowv = TRUE, Colv=TRUE, distfun = dist, hclustfun = hclust, dendrogram="both", 
+			main = "Heat Map #1", srtCol=45, col=mypalette,  # Write Column names with 45 degrees angle
+			sepwidth=c(0.01, 0.01), sepcolor="black", colsep=matrix_length, rowsep=matrix_length # Specify where to draw borders
+			)
 #########################################################
 ### Draw a levelplot
 #########################################################
-levelplot(cdata, col.regions=heat.colors, xlab = "People", ylab = "Y People", main = "Heat Map #1")
+#levelplot(cdata, col.regions=heat.colors, xlab = "People", ylab = "Y People", main = "Heat Map #1")
